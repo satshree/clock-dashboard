efe8a5f9f0878e61ff7bc5d21c914f80ba809621
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2022-09-09 10:20:49 -0500

    fixed warnings

--- components/Widget/index.js
+++ components/Widget/index.js
@@ -73,7 +73,7 @@ class WidgetContainer extends Component {
         <Head>
           <title>Clock</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&display=swap"
             rel="stylesheet"
