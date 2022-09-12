a373d49c8b8d1814fd9a7a98af1f983e1297f934
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2022-09-12 14:11:44 -0500

    updated headers

--- components/Widget/index.js
+++ components/Widget/index.js
@@ -76,12 +76,6 @@ class WidgetContainer extends Component {
       <>
         <Head>
           <title>Clock</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <div className="container">
           <div
--- pages/_document.js
+++ pages/_document.js
@@ -47,6 +47,12 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
