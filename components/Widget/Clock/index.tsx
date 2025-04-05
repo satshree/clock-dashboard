08f599c13d0c4be9358ab9274bd2abd341e70b66
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-04-04 23:05:11 -0500

    add clock widget

---
+++ components/Widget/Clock/index.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import { WeatherContext } from "@/context/WeatherContext";
+
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+
+import { getTimeAfterShiftFromUTC } from "@/utils/time";
+
+import GlobalStyle from "@/styles";
+
+export default function Clock() {
+  const weatherContext = useContext(WeatherContext);
+
+  if (!weatherContext) return;
+
+  const { weather } = weatherContext;
+
+  const [hour, setHour] = useState("");
+  const [minute, setMinute] = useState("");
+  const [ampm, setAmPm] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const time = getTimeAfterShiftFromUTC(weather.timezone);
+
+      setHour(time.hour);
+      setMinute(time.minutes);
+      setAmPm(time.ampm);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <ThemedView style={[GlobalStyle.flexCenter, { flexDirection: "row" }]}>
+      <ThemedText type="defaultSemiBold" style={{ fontSize: 225 }}>
+        {hour}:{minute}
+      </ThemedText>
+      <ThemedText type="defaultSemiBold" style={{ fontSize: 65 }}>
+        {ampm}
+      </ThemedText>
+    </ThemedView>
+  );
+}
