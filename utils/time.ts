4cb05f1e06c2cb0f39dc086c2675d09d098580dc
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-04-04 23:04:59 -0500

    add time util function

---
+++ utils/time.ts
@@ -0,0 +1,21 @@
+export function getTimeAfterShiftFromUTC(shiftInSeconds: number) {
+  const nowUTC = new Date(new Date().toUTCString());
+  const shiftedTime = new Date(nowUTC.getTime() + shiftInSeconds * 1000);
+
+  const hours = Number(shiftedTime.getUTCHours());
+  const minutes = String(shiftedTime.getUTCMinutes()).padStart(2, "0");
+
+  if (hours < 12) {
+    return {
+      hour: String(hours).padStart(2, "0"),
+      minutes,
+      ampm: "AM",
+    };
+  } else {
+    return {
+      hour: String((12 - hours) * -1),
+      minutes,
+      ampm: "PM",
+    };
+  }
+}
