d11674406093c0db386429118cbce49763ee2a7e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-04-04 22:36:50 -0500

    add capitalize util function

---
+++ utils/index.ts
@@ -0,0 +1,9 @@
+export function capitalizeWords(str: string) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
