764b487feb88ddd6383399e2a547cdbd4ced7556
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-04-04 21:54:20 -0500

    update weather api url

--- api/weather.ts
+++ api/weather.ts
@@ -1,36 +1,46 @@
+/**
+ * API Doc: https://openweathermap.org/current
+ */
+
 import openWeatherAPIRequest from ".";
 
 export type Weather = {
-  lat: number;
-  lon: number;
-  timezone: string;
-  timezone_offset: number;
-  hourly: {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  main: {
     temp: number;
     feels_like: number;
-    weather: {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }[];
-  }[];
-  daily: {
-    summary: string;
-    temp: {
-      min: number;
-      max: number;
-    };
-  }[];
+    temp_min: number;
+    temp_max: number;
+  };
+  timezone: number; // Shift in seconds from UTC
 };
 
 export const emptyWeatherData: Weather = {
-  lat: 0,
-  lon: 0,
-  timezone: "",
-  timezone_offset: 0,
-  hourly: [],
-  daily: [],
+  id: 0,
+  name: "",
+  coord: {
+    lat: 0,
+    lon: 0,
+  },
+  weather: [],
+  main: {
+    temp: 0,
+    feels_like: 0,
+    temp_min: 0,
+    temp_max: 0,
+  },
+  timezone: 0,
 };
 
 export default async function getWeather(
@@ -39,7 +49,7 @@ export default async function getWeather(
   unit: "metric" | "imperial"
 ): Promise<Weather> {
   const response = await openWeatherAPIRequest.get(
-    `/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&units=${unit}`
+    `/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}`
   );
 
   return response.data;
